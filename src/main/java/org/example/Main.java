7fca8ea487a95b86e956ae0f8c5ecd19e6d965f5
Allan J. Souza <allanjsouza@gmail.com>
2026-02-24 12:04:35 -0300

    chore: remove unused import statements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,8 @@
 package org.example;
 
-import org.example.model.Expense;
-import org.example.util.ExpenseDataLoader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
@@ -15,5 +11,6 @@ public class Main implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {}
+    public void run(String... args) throws Exception {
+    }
 }
--- src/main/java/org/example/model/Expense.java
+++ src/main/java/org/example/model/Expense.java
@@ -1,6 +1,5 @@
 package org.example.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
