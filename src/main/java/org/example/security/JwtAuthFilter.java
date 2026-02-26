00f8e9bfbb62ba1d0243a523d4520cbe904522d1
Allan J. Souza <allanjsouza@gmail.com>
2026-02-26 14:01:36 -0300

    refactor: use a custom user details service instead of default

--- src/main/java/org/example/security/JwtAuthFilter.java
+++ src/main/java/org/example/security/JwtAuthFilter.java
@@ -2,10 +2,11 @@ package org.example.security;
 
 import java.io.IOException;
 
+import org.example.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,13 +20,11 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-  private final JwtUtil jwtUtil;
-  private final UserDetailsService userDetailsService;
+  @Autowired
+  private JwtUtil jwtUtil;
 
-  public JwtAuthFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
-    this.jwtUtil = jwtUtil;
-    this.userDetailsService = userDetailsService;
-  }
+  @Autowired
+  private CustomUserDetailsService userDetailsService;
 
   @Override
   protected void doFilterInternal(
--- src/main/java/org/example/security/SecurityConfig.java
+++ src/main/java/org/example/security/SecurityConfig.java
@@ -1,22 +1,23 @@
 package org.example.security;
 
+import org.example.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+  @Autowired
+  private CustomUserDetailsService userDetailsService;
 
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
@@ -37,6 +38,7 @@ public class SecurityConfig {
                 .requestMatchers("/signup").permitAll()
                 .requestMatchers("/login").permitAll()
                 .anyRequest().authenticated())
+        .userDetailsService(userDetailsService)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
@@ -44,20 +46,5 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
-  }
-
-  @Bean
-  public UserDetailsService inMemoryUserDetailsManager() {
-    UserDetails regularUser = User.withUsername("user")
-        .password(passwordEncoder().encode("pass1234"))
-        .roles("USER")
-        .build();
-
-    UserDetails adminUser = User.withUsername("admin")
-        .password(passwordEncoder().encode("admin1234"))
-        .roles("ADMIN")
-        .build();
-
-    return new InMemoryUserDetailsManager(regularUser, adminUser);
   }
 }
