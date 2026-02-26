8a619f32ff637ed349ab42afa3dc8bf6ae371d04
Allan J. Souza <allanjsouza@gmail.com>
2026-02-26 14:02:02 -0300

    feat: return a JWT token on signup

--- src/main/java/org/example/service/SignUpService.java
+++ src/main/java/org/example/service/SignUpService.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 import org.example.model.AppUser;
 import org.example.repository.AppUserRepository;
+import org.example.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,16 +16,17 @@ public class SignUpService {
   @Autowired
   private PasswordEncoder passwordEncoder;
 
+  @Autowired
+  private JwtUtil jwtUtil;
+
   public String signUpUser(String username, String password) {
-    if (userRepo.findByUsername(username).isPresent()) {
+    if (userRepo.findByUsername(username).isPresent())
       return "Error: Username already exists";
-    }
 
     String encodedPassword = passwordEncoder.encode(password);
-
     AppUser newUser = new AppUser(username, encodedPassword);
     userRepo.save(newUser);
 
-    return "User successfully signed up!";
+    return jwtUtil.generateToken(username);
   }
 }
