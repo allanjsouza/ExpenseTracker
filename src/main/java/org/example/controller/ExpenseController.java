1c9bf531165e0cd5f5592e8542220fd96ebbaa49
Allan J. Souza <allanjsouza@gmail.com>
2026-02-27 20:08:00 -0300

    feat: make all expenses management related to the user

--- src/main/java/org/example/controller/ExpenseController.java
+++ src/main/java/org/example/controller/ExpenseController.java
@@ -2,9 +2,12 @@ package org.example.controller;
 
 import org.example.dto.ExpenseFilterParamsDTO;
 import org.example.model.Expense;
+import org.example.model.User;
 import org.example.service.ExpenseService;
+import org.example.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,48 +15,64 @@ import java.util.Optional;
 
 @RestController
 public class ExpenseController {
-    private final ExpenseService expenseService;
 
-    public ExpenseController(ExpenseService expenseService) {
+    private final ExpenseService expenseService;
+    private final UserService userService;
+
+    public ExpenseController(ExpenseService expenseService, UserService userService) {
         this.expenseService = expenseService;
+        this.userService = userService;
     }
 
     @GetMapping("/expenses")
-    public ResponseEntity<List<Expense>> listExpenses(ExpenseFilterParamsDTO filters) {
-        List<Expense> expenses = expenseService.getExpenses(filters);
+    public ResponseEntity<List<Expense>> listExpenses(Authentication auth, ExpenseFilterParamsDTO filters) {
+        User user = userService.findByUsername(auth.getName());
+        List<Expense> expenses = expenseService.listExpenses(user.getId(), filters);
+
         return ResponseEntity.ok(expenses);
     }
 
     @GetMapping("/expenses/{id}")
-    public ResponseEntity<Expense> getExpense(@PathVariable Long id) {
-        Optional<Expense> expense = expenseService.getExpense(id);
+    public ResponseEntity<Expense> getExpense(Authentication auth, @PathVariable Long id) {
+        User user = userService.findByUsername(auth.getName());
+        Optional<Expense> expense = expenseService.getExpense(user.getId(), id);
+
         return expense.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/expenses")
-    public ResponseEntity<Expense> addExpense(@RequestBody Expense expense) {
-        Expense newExpense = expenseService.addExpense(expense);
+    public ResponseEntity<Expense> addExpense(Authentication auth, @RequestBody Expense expense) {
+        User user = userService.findByUsername(auth.getName());
+        Expense newExpense = expenseService.addExpense(user.getId(), expense);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(newExpense);
     }
 
     @PutMapping("/expenses/{id}")
-    public ResponseEntity<Expense> updateExpense(@PathVariable Long id, @RequestBody Expense expense) {
-        Optional<Expense> updatedExpense = expenseService.updateExpense(id, expense);
+    public ResponseEntity<Expense> updateExpense(Authentication auth, @PathVariable Long id,
+            @RequestBody Expense expense) {
+        User user = userService.findByUsername(auth.getName());
+        Optional<Expense> updatedExpense = expenseService.updateExpense(user.getId(), id, expense);
+
         return updatedExpense.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("/expenses/{id}")
-    public ResponseEntity<Expense> deleteExpense(@PathVariable Long id) {
-        Optional<Expense> expense = expenseService.deleteExpense(id);
-        if (expense.isPresent()) {
+    public ResponseEntity<Expense> deleteExpense(Authentication auth, @PathVariable Long id) {
+        User user = userService.findByUsername(auth.getName());
+        Optional<Expense> expense = expenseService.deleteExpense(user.getId(), id);
+
+        if (expense.isPresent())
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-        }
+
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/expense_categories")
-    public ResponseEntity<List<String>> listExpenseCategories() {
-        List<String> categories = expenseService.getExpenseCategories();
+    public ResponseEntity<List<String>> listExpenseCategories(Authentication auth) {
+        User user = userService.findByUsername(auth.getName());
+        List<String> categories = expenseService.getExpenseCategories(user.getId());
+
         return ResponseEntity.ok(categories);
     }
 }
